d697b359802f601758cdd418668e25479846cf3f
Ester Costa de Souza <alwesterc@gmail.com>
2024-07-05 18:44:29 -0300

    feat: criando componente ActivityIndicator

---
+++ src/components/ActivityIndicator/ActivityIndicator.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import {
+  ActivityIndicator as RNActivityIndicator,
+  ActivityIndicatorProps,
+} from 'react-native';
+
+import {useAppTheme} from '@hooks';
+import {ThemeColors} from '@theme';
+
+interface Props extends Omit<ActivityIndicatorProps, 'color'> {
+  color: ThemeColors;
+}
+
+export function ActivityIndicator({color, ...activityIndicator}: Props) {
+  const {colors} = useAppTheme();
+
+  return <RNActivityIndicator color={colors[color]} {...activityIndicator} />;
+}
