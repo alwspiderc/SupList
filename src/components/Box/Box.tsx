8f46d4f5abf18980c49a5d29f0719e381ce5d265
Ester Costa de Souza <alwesterc@gmail.com>
2024-07-05 18:44:56 -0300

    feat: criando componente Box com Theme

---
+++ src/components/Box/Box.tsx
@@ -0,0 +1,37 @@
+import {
+  TouchableOpacity,
+  TouchableOpacityProps as RNTouchableOpacityProps,
+} from 'react-native';
+import {
+  backgroundColor,
+  BackgroundColorProps,
+  border,
+  BorderProps,
+  createBox,
+  createRestyleComponent,
+  layout,
+  LayoutProps,
+  spacing,
+  SpacingProps,
+  spacingShorthand,
+  SpacingShorthandProps,
+} from '@shopify/restyle';
+import {Theme} from '@theme';
+
+export const Box = createBox<Theme>();
+export type BoxProps = React.ComponentProps<typeof Box>;
+
+export type TouchableOpacityBoxProps = RNTouchableOpacityProps &
+  BackgroundColorProps<Theme> &
+  SpacingProps<Theme> &
+  LayoutProps<Theme> &
+  BorderProps<Theme> &
+  SpacingShorthandProps<Theme>;
+
+export const TouchableOpacityBox = createRestyleComponent<
+  TouchableOpacityBoxProps,
+  Theme
+>(
+  [backgroundColor, spacing, layout, border, spacingShorthand],
+  TouchableOpacity,
+);
---
+++ src/theme/theme.ts
@@ -0,0 +1,56 @@
+import {createTheme} from '@shopify/restyle';
+
+export const palette = {
+  greenPrimary: '#18444B',
+  greenPrimaryLight: '#68FEE4',
+  redError: '#EA3838',
+  redErrorLight: '#FEEFEE',
+
+  grayBlack: '#1A191B',
+  gray1: '#EDECEF',
+  gray2: '#E1E1E1',
+  grayWhite: '#FEFDFF',
+};
+
+export const theme = createTheme({
+  colors: {
+    ...palette,
+    primary: palette.greenPrimary,
+    background: palette.grayWhite,
+  },
+  spacing: {
+    s8: 8,
+    s14: 14,
+    s16: 16,
+    s20: 20,
+    s26: 26,
+    s40: 40,
+    s60: 60,
+  },
+  borderRadii: {
+    s16: 16,
+  },
+  textVariants: {
+    header: {
+      fontWeight: 'bold',
+      fontSize: 30,
+    },
+    smallHeader: {
+      fontWeight: 'bold',
+      fontSize: 20,
+    },
+    textMedium: {
+      fontSize: 20,
+    },
+    button: {
+      fontWeight: 'bold',
+      fontSize: 14,
+      color: 'grayWhite',
+      textTransform: 'uppercase',
+    },
+    defaults: {},
+  },
+});
+
+export type Theme = typeof theme;
+export type ThemeColors = keyof Theme['colors'];
