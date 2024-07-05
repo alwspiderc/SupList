98c87af99b51235d686305b2eab449b924483a54
Ester Costa de Souza <alwesterc@gmail.com>
2024-07-05 18:39:52 -0300

    feat: configurando path absolute

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,18 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: './src/*',
+        alias: {
+          '@components': './src/components',
+          '@hooks': './src/hooks',
+          // '@routes': './src/routes',
+          '@screens': './src/screens',
+          '@theme': './src/theme',
+        },
+      },
+    ],
+  ],
 };
