edad61430e8159d7c68a9a70de11dfa50ff0c693
Ester Costa de Souza <alwesterc@gmail.com>
2024-07-05 18:45:36 -0300

    feat: criando componente TextInput

---
+++ src/components/TextInput/TextInput.tsx
@@ -0,0 +1,75 @@
+import React, {useRef} from 'react';
+import {
+  Pressable,
+  TextInput as RNTextInput,
+  TextInputProps as RNTextInputProps,
+  TextStyle,
+} from 'react-native';
+
+import {useAppTheme} from '@hooks';
+
+import {Box, BoxProps} from '../Box/Box';
+import {Text} from '../Text/Text';
+// import {$fontFamily, $fontSizes, Text} from '../Text/Text';
+
+export interface TextInputProps extends RNTextInputProps {
+  label: string;
+  errorMessage?: string;
+  RightComponent?: React.ReactElement;
+  boxProps?: BoxProps;
+}
+
+export function TextInput({
+  label,
+  errorMessage,
+  RightComponent,
+  boxProps,
+  ...rnTextInputProps
+}: TextInputProps) {
+  const {colors} = useAppTheme();
+  const inputRef = useRef<RNTextInput>(null);
+
+  const $textInputContainer: BoxProps = {
+    flexDirection: 'row',
+    borderWidth: errorMessage ? 2 : 1,
+    borderColor: errorMessage ? 'redError' : 'grayBlack',
+    padding: 's16',
+    borderRadius: 's16',
+  };
+
+  function focusInput() {
+    inputRef.current?.focus();
+  }
+
+  return (
+    <Box {...boxProps}>
+      <Pressable onPress={focusInput}>
+        <Text marginBottom="s14">{label}</Text>
+        <Box {...$textInputContainer}>
+          <RNTextInput
+            autoCapitalize="none"
+            ref={inputRef}
+            placeholderTextColor={colors.grayBlack}
+            style={$textInputStyle}
+            {...rnTextInputProps}
+          />
+          {RightComponent && (
+            <Box justifyContent="center" ml="s16">
+              {RightComponent}
+            </Box>
+          )}
+        </Box>
+        {errorMessage && <Text color="redError">{errorMessage}</Text>}
+      </Pressable>
+    </Box>
+  );
+}
+
+const $textInputStyle: TextStyle = {
+  padding: 0,
+  flexGrow: 1,
+  flexShrink: 1,
+
+  // fontFamily: $fontFamily.regular,
+  // ...$fontSizes.paragraphMedium,
+};
