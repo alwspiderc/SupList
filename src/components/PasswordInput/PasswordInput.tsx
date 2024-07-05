dc003121009b258213a6e5bfb9e0aa15ca06a9f1
Ester Costa de Souza <alwesterc@gmail.com>
2024-07-05 18:45:50 -0300

    feat: criando componente PasswordInput

---
+++ src/components/PasswordInput/PasswordInput.tsx
@@ -0,0 +1,27 @@
+import React, {useState} from 'react';
+
+import {TextInput, TextInputProps, Icon} from '@components';
+
+export type PasswordInputProps = Omit<TextInputProps, 'RightComponent'>;
+
+export function PasswordInput(props: PasswordInputProps) {
+  const [isSecureTextEntry, setIsSecureTextEntry] = useState(true);
+
+  function toggleSecureTextEntry() {
+    setIsSecureTextEntry(prev => !prev);
+  }
+
+  return (
+    <TextInput
+      {...props}
+      secureTextEntry={isSecureTextEntry}
+      RightComponent={
+        <Icon
+          onPress={toggleSecureTextEntry}
+          color="grayBlack"
+          name={isSecureTextEntry ? 'eyeOn' : 'eyeOff'}
+        />
+      }
+    />
+  );
+}
